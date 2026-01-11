c34293ff1c043a3202406b2b53bad904a56d6e90
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-11 23:05:07 +0400

    feat: add class to section element

--- script/components/MainGridContent.js
+++ script/components/MainGridContent.js
@@ -1,4 +1,8 @@
 export const MainGridContent = () => {
   const section_element = document.getElementById("main-grid-content");
+  section_element.classList.add("grid");
   section_element.textContent = "test connecting to js";
+  const div_element = document.createElement("div");
+  div_element.textContent = "new div";
+  section_element.appendChild(div_element);
 };
