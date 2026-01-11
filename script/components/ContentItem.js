f5fa9d13c93c34594e093c1924150958a686f4a2
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-12 01:17:06 +0400

    feat: add full content in grid from MAIN_GRID_CONTENT

--- script/components/ContentItem.js
+++ script/components/ContentItem.js
@@ -3,11 +3,20 @@ import { SingleItem } from "./SingleItem.js";
 export default function ContentItem(item) {
   const div_element = document.createElement("div");
   div_element.classList.add("grid-column");
+
   const h2_element = document.createElement("h2");
   h2_element.classList.add("subsection-header");
   h2_element.textContent = item.title;
   div_element.appendChild(h2_element);
-  const single_item_element = SingleItem({ data: item.data[1] });
-  div_element.appendChild(single_item_element);
+
+  const container = document.createElement("div");
+  container.className = "grid-column-container";
+
+  item.data.forEach((element) => {
+    const single_item_element = SingleItem({ data: element });
+    container.appendChild(single_item_element);
+  });
+
+  div_element.appendChild(container);
   return div_element;
 }
