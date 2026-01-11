8c04f17db16e303fbc634c46a28e5b75bd51b135
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-11 22:50:16 +0400

    feat: manipulate data via dom

---
+++ script/components/MainGridContent.js
@@ -0,0 +1,4 @@
+export const MainGridContent = () => {
+  const section_element = document.getElementById("main-grid-content");
+  section_element.textContent = "test connecting to js";
+};
--- script/index.js
+++ script/index.js
@@ -1,5 +1,8 @@
+import { MainGridContent } from "./components/MainGridContent.js";
+
 function main() {
   console.log("main js function");
+  MainGridContent();
 }
 
 main();
