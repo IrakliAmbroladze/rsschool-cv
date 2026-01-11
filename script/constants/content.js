0deac741934260f4cdbe91e9dcefa387b0e66b34
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-11 22:31:14 +0400

    feat: create constant data for Experience

---
+++ script/constants/content.js
@@ -0,0 +1,17 @@
+export const EXPERIENCE = {
+  title: "Experience",
+  data: [
+    {
+      title: "Softare Engineer",
+      subtitle: "(Freelance / Personal Projects) — 2020 – Present",
+      description:
+        "Developing web applications and business management systems. Specializing in React.js ecosystem and database-driven applications. Creating solutions for small to medium businesses, particularly in service industries.",
+    },
+    {
+      title: "Founder & Director",
+      subtitle: "Killers (Pest Control & Sanitation Services) 2014 – Present",
+      description:
+        "Founded and successfully operate service-based business with 10+ recurring clients. Developed internal ERP system to manage operations, scheduling, and finances. Applied technical skills to automate business processes and improve efficiency.",
+    },
+  ],
+};
